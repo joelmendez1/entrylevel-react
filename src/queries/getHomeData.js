7fb7f0bda84bb7f650f2f087d6edc895d9306cf0
Joel Elias Méndez <67445275+joelmendez1@users.noreply.github.com>
2022-07-15 16:54:12 -0300

    add session storage (#1)

--- src/queries/getHomeData.js
+++ src/queries/getHomeData.js
@@ -1,19 +1,31 @@
 import { client, Field, Query } from "@tilework/opus";
+import { saveData } from '../utils/sessionStorage'
 
 client.setEndpoint("http://localhost:4000/graphql"); 
 
-const getCategoriesName = async () => { 
-    const queryCategoriesName = new Query("categories", true)
-    .addField('name', true)
 
-    return await client.post(queryCategoriesName)   
+const getCategoriesName = async () => { 
+    const field = 'name';
+
+    const queryCategoriesName = new Query("categories", true)
+    .addField(field, true);
+    const response  = await client.post(queryCategoriesName);
+
+    saveData(field, response);
+
+    return response;
 }
 
 const getProducts = async () => {
-    const queryProducts = new Query("categories", true)
-        .addField(new Field('products', true).addFieldList(['id', 'name', 'inStock', 'gallery', 'description', 'category', 'attributes{id, name, type, items{displayValue, value, id}}', 'prices{currency{label, symbol}, amount}']))
+    const field = 'products';
 
-    return await client.post(queryProducts)
+    const queryProducts = new Query("categories", true)
+        .addField(new Field(field, true).addFieldList(['id', 'name', 'inStock', 'gallery', 'description', 'category', 'attributes{id, name, type, items{displayValue, value, id}}', 'prices{currency{label, symbol}, amount}']));
+    const response  = await client.post(queryProducts);
+
+    saveData(field, response);
+
+    return response;
 }
 
 export { getCategoriesName, getProducts }

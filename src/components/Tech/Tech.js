175d7bbe6d5fd9de8659f194187a9cef74a08d68
Joel Elias Méndez <67445275+joelmendez1@users.noreply.github.com>
2022-07-15 22:14:09 -0300

    add remaining restantes (#2)

--- src/components/Tech/Tech.js
+++ src/components/Tech/Tech.js
@@ -1,13 +1,45 @@
 import React from "react";
-
+import { Product } from '../Product/Products';
+import { checkSessionData } from '../../utils/sessionStorage';
+import { getAll } from '../../queries/getHomeData';
 class Tech extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        }
+    };
+
+    componentDidMount() {
+        {
+            (JSON.parse(sessionStorage.getItem('all')) ? checkSessionData('all') : getAll())
+                .then(res => {
+                    const tech = res.categories.filter(category => category.name === 'tech');
+                    this.setState({
+                        products: tech
+                    })
+                })
+                .catch(error => {
+                    console.error('An error has ocurred: ', error);
+                })
+        }
+    }
+
     render() {
+        const { products } = this.state;
         return (
             <div>
-                Tech
+                {products.map(element => {
+                    return element.products.map((product, index) => (
+                        <Product key={index}
+                        name={product.name}
+                        img={product.gallery[0]}
+                        stock={product.inStock}/>
+                    ))
+                })}
             </div>
         )
-    }
+    };
 }
 
 export { Tech }

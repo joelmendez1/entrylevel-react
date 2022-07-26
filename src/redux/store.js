21cb5936c68b6c8ab4a595a6af7a5c8c6356a415
Joel Mendez <joel.mendez@outlook.es>
2022-07-26 14:52:40 -0300

    fix persist issues

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,29 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { navReducer } from './nav/navReducer';
 import { currencyReducer } from "./currency/currencyReducer";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from "redux-persist/lib/storage";
 
-const persistConfig = {
-    key: 'persist-key',
-    storage
+const navPersistConfig = {
+    key: 'nav-key',
+    storage,
+    blacklist: ['currency-key']
 };
-const navPersistReducer = persistReducer(persistConfig, navReducer);
-const currencyPersistReducer = persistReducer(persistConfig, currencyReducer)
+const currencyPersistConfig = {
+  key: 'currency-key',
+  storage,
+  blacklist: ['nav-key']
+};
+const navPersistReducer = persistReducer(navPersistConfig, navReducer);
+const currencyPersistReducer = persistReducer(currencyPersistConfig, currencyReducer)
 const rootReducer = combineReducers({navPersistReducer, currencyPersistReducer})
-const store = configureStore({ reducer: rootReducer});
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    })
+});
 const persistor = persistStore(store);
 
 export { store, persistor }

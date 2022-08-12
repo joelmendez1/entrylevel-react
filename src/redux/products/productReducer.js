5ab14e3cb2fe62c338b151aefdae2b39b1c4454a
Joel Mendez <joel.mendez@outlook.es>
2022-08-12 11:35:35 -0300

    add object comparator to product reducer

--- src/redux/products/productReducer.js
+++ src/redux/products/productReducer.js
@@ -11,17 +11,24 @@ const arrayCompare = (arr1, arr2) => {
     return arr1.join() === arr2.join();
 }
 
+const objectCompare = (obj1, obj2) => {
+    if(arrayCompare(Object.keys(obj1), Object.keys(obj2))) {
+        if(arrayCompare(Object.values(obj1), Object.values(obj2))) {
+            return true;
+        }
+    }
+    return false;
+}
+
 const incrementor = (arr, action) => {
-    const currentProductAttributesKeys = Object.keys(action.product.selectedAttributes);
+    const currentProductAttributes = action.product.selectedAttributes;
 
     for(let i = 0; i < arr.length; i++) {
         const product = arr[i]
         if(product.name === action.product.name) {
-            if(arrayCompare(currentProductAttributesKeys, Object.keys(product.selectedAttributes))) {
-                if(arrayCompare(Object.values(action.product.selectedAttributes), Object.values(product.selectedAttributes))) {
-                    product.count += 1
-                    return true
-                }
+            if(objectCompare(currentProductAttributes, product.selectedAttributes)) {
+                product.count += 1;
+                return true;
             }
         }
     }
@@ -54,14 +61,20 @@ const productsReducer = (state = initialState, action) => {
                 totalProducts: state.totalProducts + 1
             }
         case DECREMENT:
+            const currentProductAttributes= action.product.selectedAttributes;
+
             for(let i = 0; i < products.length; i++) {
                 const product = products[i];
 
-                if(product.id === action.product.id && product.count >= 1) {
-                    product.count -= 1;
-                }
-                if(product.count === 0) {
-                   products = products.filter((item) => item.count > 0)
+                if(product.id === action.product.id) {
+                    if(objectCompare(currentProductAttributes, product.selectedAttributes)) {
+                        if(product.count > 0) {
+                            product.count -= 1;
+                        }
+                        if(product.count === 0) {
+                           products = products.filter((item) => item.count > 0);
+                        }
+                    }
                 }
             }
 

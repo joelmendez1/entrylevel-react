60e3bfe7d06b885e17c300fb1741e4ef20a52fdf
Joel Mendez <joel.mendez@outlook.es>
2022-08-26 14:38:29 -0300

    render routes dynamically

--- src/App.js
+++ src/App.js
@@ -5,31 +5,71 @@ import All from "./components/All/All";
 import Nav from "./components/Nav/Nav";
 import ProductDescription from "./components/ProductDescription/ProductDescription";
 import CartPage from "./components/Cart/CartPage/CartPage";
+import { checkSessionData } from "./utils/sessionStorage";
+import { getCategoriesName } from "./queries/getCategoriesName";
+import { Loader } from "./components/Loader/Loader";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    (JSON.parse(sessionStorage.getItem("categories"))
+      ? checkSessionData("categories")
+      : getCategoriesName("categories")
+    )
+      .then((res) => {
+        this.setState({
+          categories: res.categories,
+        });
+      })
+      .catch((error) => {
+        console.error("An error has ocurred: ", error);
+      })
+      .finally(() => {
+        this.setState({
+          loading: false,
+        });
+      });
+  }
+
   render() {
+    const { categories, loading } = this.state;
+
     return (
       <BrowserRouter>
         <Nav />
-        <Routes>
-          <Route path="/" element={<All to="/all" />} />
-          <Route path="/all" element={<All to="/" />}></Route>
-          <Route path="/clothes" element={<All />}></Route>
-          <Route path="/tech" element={<All />}></Route>
-          <Route
-            path="/product/:product"
-            element={<ProductDescription />}
-          ></Route>
-          <Route path="/cart" element={<CartPage />}></Route>
-          <Route
-            path="*"
-            element={
-              <div>
-                <h2>404 Page not found</h2>
-              </div>
-            }
-          />
-        </Routes>
+        {!loading ? (
+          <Routes>
+            {categories.map((category) => (
+              <Route
+                key={`route-${category.name}`}
+                path={`/${category.name}`}
+                element={<All />}
+              ></Route>
+            ))}
+            <Route
+              path="/product/:product"
+              element={<ProductDescription />}
+            ></Route>
+            <Route path="/cart" element={<CartPage />}></Route>
+            <Route
+              path="*"
+              element={
+                <div>
+                  <h2>404 Page not found</h2>
+                </div>
+              }
+            />
+          </Routes>
+        ) : (
+          <Loader />
+        )}
       </BrowserRouter>
     );
   }
